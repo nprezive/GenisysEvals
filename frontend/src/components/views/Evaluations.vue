debb58e72db8ea141ae2626f272be12970795378
Taylor Earl <soccerdude28844@gmail.com>
2018-02-15 17:49:57 -0700

    Mock Up for Evals Taken

--- frontend/src/components/views/Evaluations.vue
+++ frontend/src/components/views/Evaluations.vue
@@ -47,36 +47,128 @@
           <q-tab-pane name="tab-2" class="p-0">
             <q-toolbar inverted color="dark" class="bg-light">
               <q-toolbar-title>
-                My Completion Lists
+                Students Who Have Taken Course Summaries
+                 <span slot="subtitle">
+                   If less than 10 surveys have been taken, none will be listed to preserve anonymity.
+                 </span>
               </q-toolbar-title>
-              <q-fab color="primary" active-icon="fa-times" icon="fa-cogs" direction="left">
-                <q-fab-action color="negative" @click="toast('alarm')" icon="fa-question">
-                  <q-tooltip anchor="center left" self="center right" :offset="[20, 0]">Need Help?</q-tooltip>
-                </q-fab-action>
-                <q-fab-action color="primary" @click="toast('alarm')" icon="fa-user">
-                  <q-tooltip anchor="center left" self="center right" :offset="[20, 0]">Create A Evaluation</q-tooltip>
-                </q-fab-action>
-              </q-fab>
             </q-toolbar>
             <div class="row clearfix p-1">
               <div class="col-md-12 col-lg-4 clearfix">
                 <q-card>
                   <q-card-title class="bg-primary">
-                    <b>My Completion Lists?</b>
+                    <b>Current Semester</b>
                   </q-card-title>
                   <q-card-main class="card-block pt-2">
-                    <p>
-                      Duplicate what is in the presentation slides. Who have taken surveys.
-                    </p>
-                    <p>I was copied from Proctoring.vue</p>
+                    <q-list no-border>
+                      <q-item>
+                        <q-item-main>
+                          <q-item-tile label>CS1400 ONL Spring 18</q-item-tile>
+                           <q-item-tile sublabel lines="2">
+                              15/33 Students Have Responded
+                          </q-item-tile>
+                        </q-item-main>
+                      </q-item>
+                      <q-item>
+                        <q-item-main>
+                          <q-item-tile label>CS1410 WSD Spring 18 (this course is not being evaluated)</q-item-tile>
+                          <q-item-tile sublabel lines="2">
+                              No Students Have Responded
+                          </q-item-tile>
+                        </q-item-main>
+                      </q-item>
+                    </q-list>
+                  </q-card-main>
+
+                </q-card>
+              </div>
+              <div class="col-md-12 col-lg-4 clearfix">
+                <q-card>
+                  <q-card-title class="bg-primary">
+                    <b>Previous Semesters</b>
+                  </q-card-title>
+                  <q-card-main class="card-block pt-2">
+                     <q-list no-border>
+                      <q-item>
+                        <q-item-main>
+                          <q-item-tile label><q-btn @click="$refs.basicModal.open()">Fall 2017 (3 Courses)</q-btn></q-item-tile>
+                        </q-item-main>
+                      </q-item>
+                      <q-item>
+                        <q-item-main>
+                          <q-item-tile label @click="$refs.basicModal2.open()"><q-btn>Summer 2017 (2 Courses)</q-btn></q-item-tile>
+                        </q-item-main>
+                      </q-item>
+                    </q-list>
                   </q-card-main>
                 </q-card>
               </div>
-              <div class="col-md-12 col-lg-8 clearfix">
 
+              <div class="col-md-12 col-lg-4 clearfix">
+                <q-card>
+                  <q-card-title class="bg-primary">
+                    <b>Future Semesters</b>
+                  </q-card-title>
+                  <q-card-main class="card-block pt-2">
+                     <q-list no-border>
+                      <q-item>
+                        <q-item-main>
+                          <q-item-tile label><q-btn>Summer 2018 (3 Courses)</q-btn></q-item-tile>
+                        </q-item-main>
+                      </q-item>
+                    </q-list>
+                  </q-card-main>
+                </q-card>
               </div>
+
             </div>
           </q-tab-pane>
+
+          <q-modal ref="basicModal" :content-css="{padding: '50px'}">
+            <h4>Fall 2017</h4>
+            <q-list no-border>
+                      <q-item>
+                        <q-item-main>
+                          <q-item-tile label>CS1400 ONL Fall 17</q-item-tile>
+                           <q-item-tile sublabel lines="2">
+                              30/33 Students Have Responded
+                          </q-item-tile>
+                        </q-item-main>
+                      </q-item>
+                      <q-item>
+                        <q-item-main>
+                          <q-item-tile label>CS1410 WSD Fall 17 (this course is not being evaluated)</q-item-tile>
+                          <q-item-tile sublabel lines="2">
+                              No Students Have Responded
+                          </q-item-tile>
+                        </q-item-main>
+                      </q-item>
+                    </q-list>
+            <q-btn color="primary" @click="$refs.basicModal.close()">Close</q-btn>
+          </q-modal>
+
+          <q-modal ref="basicModal2" :content-css="{padding: '50px'}">
+            <h4>Summer 2017</h4>
+            <q-list no-border>
+                      <q-item>
+                        <q-item-main>
+                          <q-item-tile label>CS1400 ONL Summer 17</q-item-tile>
+                           <q-item-tile sublabel lines="2">
+                              30/33 Students Have Responded
+                          </q-item-tile>
+                        </q-item-main>
+                      </q-item>
+                      <q-item>
+                        <q-item-main>
+                          <q-item-tile label>CS1410 WSD Summer 17 (this course is not being evaluated)</q-item-tile>
+                          <q-item-tile sublabel lines="2">
+                              No Students Have Responded
+                          </q-item-tile>
+                        </q-item-main>
+                      </q-item>
+                    </q-list>
+            <q-btn color="primary" @click="$refs.basicModal2.close()">Close</q-btn>
+          </q-modal>
 
 
           <q-tab label="My Rankings" slot="title"
