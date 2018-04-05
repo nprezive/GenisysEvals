f6f97d29aa1208cd7234643108193d4e15f164bf
Taylor Earl <soccerdude28844@gmail.com>
2018-04-04 18:02:30 -0600

    Added conditional checkboxes with more responsive layout.

--- frontend/src/components/views/evaluations/MyReports.vue
+++ frontend/src/components/views/evaluations/MyReports.vue
@@ -8,28 +8,49 @@
     </q-toolbar>
     <div class="row clearfix p-1">
       <div class="col-md-12 col-lg-12 clearfix">
-        <q-list  v-for="sem in courses">
+        <q-list  v-for="sem in semData.s" dense="true">
           <q-collapsible :label="sem.semester">
             <q-card>
               <q-card-main class="card-block pt-2">
-                <table style="width: 80%; display:inline;">
-                  <tr>
-                    <th style="width: 25%">Course</th>
-                    <th style="width: 25%; text-align:center">Should This Class be Evaluated</th>
-                    <th style="width: 25%; text-align:center">Share with Dean/Chair</th>
-                    <th style="width: 25%; text-align:center">Public Access</th>
-                  </tr>
-                  <tr v-for="c in sem.courses">
-                    <td>{{c.title}}<br><i>25/30 Have Responded</i></td>
-                    <td align="center"><input type="checkbox"></td>
-                    <td align="center"><input type="checkbox"></td>
-                    <td align="center"><input type="checkbox"></td>
-                  </tr>
-                </table>
-                <div style="float:right; width:20%;">
+                <div class="row">
+                <div class="col-md-8 col-lg-8 clearfix">
+                  <div class="row">
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3 clearfix" style="text-align:center;">
+                      <h6>Course</h6>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3 clearfix" style="text-align:center;">
+                      <h6>Evaluate</h6>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3 clearfix" style="text-align:center;">
+                      <h6>Dean/Chair Access</h6>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3 clearfix" style="text-align:center;">
+                      <h6>Public</h6>
+                    </div>
+                  </div>
+                  <div class="row" v-for="c in sem.classes">
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3 clearfix" style="text-align:center;">
+                      {{c.className}} <br>
+                      <i v-if="!c.isCourseEvaluated">Course is not evaluated</i>
+                      <i v-else-if="c.taken > 10">{{c.taken}}/{{c.total}} Have Responded</i>
+                      <i v-else-if="c.taken < 10">Less than 10 Students Responded</i>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3 clearfix" style="text-align:center;">
+                      <q-checkbox v-model="c.isCourseEvaluated"/>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3 clearfix" style="text-align:center;">
+                      <q-checkbox v-if="c.isCourseEvaluated" v-model="c.isSharedDeans"/>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3 clearfix" style="text-align:center;">
+                      <q-checkbox v-if="c.isSharedDeans" v-model="c.isPublic"/>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 col-lg-4 clearfix">
                   <p>By selecting the button below, you are agreeing to send the settings configured for this semester to the Dean or Chair for review.</p>
                   <q-btn inline color="primary">Lock</q-btn>
                 </div>
+              </div>
               </q-card-main>
             </q-card>
           </q-collapsible>
@@ -42,116 +63,145 @@
 <script>
 export default {
   name:'mydefaults',
-  computed: {
-    courses() {
-      let apicall = [
-        {
-          semester: 'Spring 2017',
-          courses:
-            [
-              {
-                title: 'CS 2420',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3000',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3100',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3200',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 4110',
-                description: "blah blah blah"
-              }
-            ]
+  data () {
+      return {
+        msg: 'Proctoring',
+        semData: {
+          s:[
+            {
+              id: 12,
+              semester: 'Spring 2018',
+              isActive: true,
+              classes: [
+                {
+                  id: 1234,
+                  crn: '22325',
+                  className: 'CS1400',
+                  taken: '22',
+                  total: '35',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                },
+                {
+                  id: 1233,
+                  crn: '22145',
+                  className: 'CS1410',
+                  taken: '19',
+                  total: '25',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                },
+                {
+                  id: 12334,
+                  crn: '22323',
+                  className: 'CS3100',
+                  taken: '18',
+                  total: '22',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                },
+                {
+                  id: 12334,
+                  crn: '22323',
+                  className: 'CS4110',
+                  taken: '9',
+                  total: '22',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                }
+              ]
+            },
+            {
+              id: 13,
+              semester: 'Fall 2017',
+              isActive: false,
+              classes: [
+                {
+                  id: 1234,
+                  crn: '22325',
+                  className: 'CS1400',
+                  taken: '22',
+                  total: '35',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                },
+                {
+                  id: 1233,
+                  crn: '22145',
+                  className: 'CS1410',
+                  taken: '19',
+                  total: '25',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                },
+                {
+                  id: 12334,
+                  crn: '22323',
+                  className: 'CS3100',
+                  taken: '18',
+                  total: '22',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                }
+              ]
+            },
+            {
+              id: 14,
+              semester: 'Spring 2017',
+              isActive: false,
+              classes: [
+                {
+                  id: 1234,
+                  crn: '22325',
+                  className: 'CS1400',
+                  taken: '22',
+                  total: '35',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                },
+                {
+                  id: 1233,
+                  crn: '22145',
+                  className: 'CS1410',
+                  taken: '19',
+                  total: '25',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                },
+                {
+                  id: 12334,
+                  crn: '22323',
+                  className: 'CS3100',
+                  taken: '18',
+                  total: '22',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                },
+                {
+                  id: 123343,
+                  crn: '223232',
+                  className: 'CS3110',
+                  taken: '13',
+                  total: '22',
+                  isCourseEvaluated: false,
+                  isPublic: false,
+                  isSharedDeans: false
+                }
+              ]
+            }
+          ]
         },
-        {
-          semester: 'Summer 2017',
-          courses:
-            [
-              {
-                title: 'CS 2420',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3000',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3100',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3200',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 4110',
-                description: "blah blah blah"
-              }
-            ]
-        },
-        {
-          semester: 'Fall 2017',
-          courses:
-            [
-              {
-                title: 'CS 2420',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3000',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3100',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3200',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 4110',
-                description: "blah blah blah"
-              }
-            ]
-        },
-        {
-          semester: 'Spring 2018',
-          courses:
-            [
-              {
-                title: 'CS 2420',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3000',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3100',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 3200',
-                description: "blah blah blah"
-              },
-              {
-                title: 'CS 4110',
-                description: "blah blah blah"
-              }
-            ]
-        }
-      ]
-      return apicall
+      }
     }
-  }
 }
 </script>
