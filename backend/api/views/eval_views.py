9821c5cf67e358929f0235c080522873a939e3b6
Josh Auton <Josh@prenticeworx.com>
2018-04-19 19:05:09 -0600

    Fixed Evals View

--- backend/api/views/eval_views.py
+++ backend/api/views/eval_views.py
@@ -34,7 +34,7 @@ class EvalViewSet(BaseModelViewSet):
 def getMyEvals(request, pk=None):
     try:
         user = request.user.id
-        evals = Evaluation.objects.filter(user=224).all()
-        return HttpResponse(evals)
+        queryset = Evaluation.objects.filter(user=user).all()
+        return HttpResponse(queryset)
     except Exception as e:
         return HttpResponse("An error occurred: {}".format(e.args[0]), status=418)
