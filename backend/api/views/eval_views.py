2087c137665c271578ab0a9cbe668506635496e5
nprezive <npreszler@gmail.com>
2018-04-25 22:10:43 -0600

    API endpoint of getmyevals returns a hardcoded string (for testing purposes). MyReports populates with data from the API call.

--- backend/api/views/eval_views.py
+++ backend/api/views/eval_views.py
@@ -34,7 +34,142 @@ class EvalViewSet(BaseModelViewSet):
 def getMyEvals(request, pk=None):
     try:
         user = request.user.id
-        queryset = list(Evaluation.objects.filter(user=user).all().values())
+        # queryset = list(Evaluation.objects.filter(user=user).all().values())
+        queryset = """{
+          "s":[
+            {
+              "id": 12,
+              "semester": "Spring 2018",
+              "isActive": true,
+              "classes": [
+                {
+                  "id": 1234,
+                  "crn": "22325",
+                  "className": "CS1400",
+                  "taken": "22",
+                  "total": "35",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                },
+                {
+                  "id": 1233,
+                  "crn": "22145",
+                  "className": "CS1410",
+                  "taken": "19",
+                  "total": "25",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                },
+                {
+                  "id": 12334,
+                  "crn": "22323",
+                  "className": "CS3100",
+                  "taken": "18",
+                  "total": "22",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                },
+                {
+                  "id": 12334,
+                  "crn": "22323",
+                  "className": "CS4110",
+                  "taken": "9",
+                  "total": "22",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                }
+              ]
+            },
+            {
+              "id": 13,
+              "semester": "Fall 2017",
+              "isActive": false,
+              "classes": [
+                {
+                  "id": 1234,
+                  "crn": "22325",
+                  "className": "CS1400",
+                  "taken": "22",
+                  "total": "35",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                },
+                {
+                  "id": 1233,
+                  "crn": "22145",
+                  "className": "CS1410",
+                  "taken": "19",
+                  "total": "25",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                },
+                {
+                  "id": 12334,
+                  "crn": "22323",
+                  "className": "CS3100",
+                  "taken": "18",
+                  "total": "22",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                }
+              ]
+            },
+            {
+              "id": 14,
+              "semester": "Spring 2017",
+              "isActive": false,
+              "classes": [
+                {
+                  "id": 1234,
+                  "crn": "22325",
+                  "className": "CS1400",
+                  "taken": "22",
+                  "total": "35",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                },
+                {
+                  "id": 1233,
+                  "crn": "22145",
+                  "className": "CS1410",
+                  "taken": "19",
+                  "total": "25",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                },
+                {
+                  "id": 12334,
+                  "crn": "22323",
+                  "className": "CS3100",
+                  "taken": "18",
+                  "total": "22",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                },
+                {
+                  "id": 123343,
+                  "crn": "223232",
+                  "className": "CS3110",
+                  "taken": "13",
+                  "total": "22",
+                  "isCourseEvaluated": false,
+                  "isPublic": false,
+                  "isSharedDeans": false
+                }
+              ]
+            }
+          ]
+        }"""
         return HttpResponse(queryset)
     except Exception as e:
         return HttpResponse("An error occurred: {}".format(e.args[0]), status=418)
