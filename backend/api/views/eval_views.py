d03c5474bd35c7a833924fd8b94e92e63622593d
Josh Auton <Josh@prenticeworx.com>
2018-04-26 19:24:44 -0600

    Fixed Evals View

--- backend/api/views/eval_views.py
+++ backend/api/views/eval_views.py
@@ -34,7 +34,7 @@ class EvalViewSet(BaseModelViewSet):
 def getMyEvals(request, pk=None):
     try:
         user = request.user.id
-        queryset = Evaluation.objects.filter(user=user).order_by('semester_id').all().values()
+        queryset = Evaluation.objects.filter(user=user).order_by('semester_id')
         obj = EvaluationSerializer(queryset)
         lastid = 0
         semester = {}
